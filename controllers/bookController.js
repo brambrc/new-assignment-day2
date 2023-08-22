705ef96c356796e101ca63dc89a6574b2b260f42
brcbram <anggiet@nikel.com>
2023-08-22 22:26:46 +0700

    update

---
+++ controllers/bookController.js
@@ -0,0 +1,24 @@
+const BookModel = require('../models/bookModel');
+
+function getBooks(req, res) {
+    
+}
+
+function addNewBook(req, res) {
+    
+}
+
+function updateExistingBook(req, res) {
+   
+}
+
+function removeBook(req, res) {
+   
+}
+
+module.exports = {
+    getBooks,
+    addNewBook,
+    updateExistingBook,
+    removeBook
+};
