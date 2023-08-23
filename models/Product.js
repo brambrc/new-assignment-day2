d8469e24d97d703480f49194690272168da37763
Fannn03 <fann.fann771@gmail.com>
2023-08-23 07:53:41 +0700

    create method findall

---
+++ models/Product.js
@@ -0,0 +1,28 @@
+let data = [
+    {
+        id: 1,
+        name: 'Air Mineral',
+        price: 5000,
+        stock: 250
+    },
+    {
+        id: 2,
+        name: 'Coca Cola',
+        price: 6000,
+        stock: 223
+    },
+    {
+        id: 3,
+        name: 'Oreo Supreme',
+        price: 20000,
+        stock: 117
+    }
+]
+
+const findAll = () => {
+    return data
+}
+
+module.exports = {
+    findAll
+}
