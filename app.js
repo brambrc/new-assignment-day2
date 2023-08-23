a186051ecb2d550883c6d181bbc4aca2b4ab2d99
Fannn03 <fann.fann771@gmail.com>
2023-08-23 14:55:23 +0700

    separate app listen for testing

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyparser = require('body-parser')
 
 const app = express();
-const port = 3000;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}))
@@ -11,6 +10,4 @@ const ProductRouter = require('./routes/ProductRouter')
 
 app.use('/products', ProductRouter)
 
-app.listen(port, () => {
-    console.log(`Bookstore MVC API running at http://localhost:${port}`);
-});
+module.exports = app
