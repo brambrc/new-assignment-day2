2caa4202d2ba1cb84c6e26bc05be3d1bfabb65b2
Fannn03 <fann.fann771@gmail.com>
2023-08-23 08:26:44 +0700

    create middleware for post data

---
+++ middleware/ProductMiddleware.js
@@ -0,0 +1,33 @@
+const create = (req, res, next) => {
+
+    const {name, price, stock} = req.body
+    let messages = {}
+
+    if(!name || !name.length) messages.name = 'Name cannot be null'
+    if(!price || !price.length) messages.price= 'Price cannot be null'
+    if(!stock || !stock.length) messages.stock = 'Stock cannot be null'
+    if(isNaN(Number(price))) messages.price = 'Price must be a number'
+    if(isNaN(Number(stock))) messages.stock = 'Stock must be a number'
+
+    const validation = isEmptyObject(messages)
+    if(!validation) return res.status(400).json({
+        result: 'error',
+        message: messages
+    })
+
+    next()
+
+}
+
+function isEmptyObject(object){
+    for(let value in object){
+        if(Object.hasOwn(object, value)){
+            return false
+        }
+    }
+    return true
+}
+
+module.exports = {
+    create
+}
